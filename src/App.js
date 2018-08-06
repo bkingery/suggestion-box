bbdde8071a20b60905b08a0366f2f66e543dc5bf
Brian Kingery <brian.kingery@domo.com>
2018-08-06 16:57:10 -0600

    data call

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
   uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      // eslint-disable-next-line
       var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
@@ -31,6 +32,14 @@ class App extends Component {
       prevState.suggestions.sort((a, b) => b.totalPlusOne - a.totalPlusOne);
       return { suggestions: prevState.suggestions };
     });
+  }
+
+  componentDidMount() {
+    fetch('/data/v1/suggestions')
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json);
+      });
   }
 
   render() {
